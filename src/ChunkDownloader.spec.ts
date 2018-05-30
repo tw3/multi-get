459c203bf86a6e38d23bda2ad0549d489eca395a
Ted Weatherly <ted.weatherly@gmail.com>
2018-05-30 00:36:46 -0500

    Improve tests

--- src/ChunkDownloader.spec.ts
+++ src/ChunkDownloader.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import 'mocha';
 import { Observable } from 'rxjs';
+import { StringDecoder, NodeStringDecoder } from 'string_decoder';
 
 import { ChunkDownloader } from './ChunkDownloader';
 import { ChunkByteRange } from './model/ChunkByteRange';
@@ -19,4 +20,33 @@ describe('ChunkDownloader.getSource()', () => {
     expect(chunkData$).to.instanceOf(Observable);
   });
 
+  it('should return yield a correct value', (done) => {
+    const url: string = 'https://dog.ceo/api/breed/affenpinscher/images/random';
+    const chunkByteRange: ChunkByteRange = {
+      start: 0,
+      end: 40
+    };
+    const chunkDownloader: ChunkDownloader = new ChunkDownloader(url, chunkByteRange);
+    const chunkData$: Observable<ChunkData> = chunkDownloader.getSource();
+    chunkData$.subscribe((chunkData: ChunkData) => {
+      check(done, () => {
+        const actualRange: ChunkByteRange = chunkData.range;
+        const expectedStr: string = '{"status":"success","message":"https:\\/\\/';
+        const stringDecoder: NodeStringDecoder = new StringDecoder('utf8');
+        const actualStr: string = stringDecoder.write(chunkData.blob);
+        expect(actualStr).to.equal(expectedStr);
+        expect(actualRange).to.equal(chunkByteRange);
+      } );
+    });
+  });
+
 });
+
+function check(done: (e?: any) => void, f: () => void): void {
+  try {
+    f();
+    done();
+  } catch (e) {
+    done(e);
+  }
+}
